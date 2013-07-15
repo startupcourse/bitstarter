6294af5c40af208c78b55481ec2b595ea7a192a4
unknown <startup.course.aws@gmail.com>
2013-07-15 20:43:59 +0100

    Edited index.html

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var http = require('http');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+/*var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http:\\www.google.com";*/
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -46,12 +48,12 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+	var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
-    }
+	}
     return out;
 };
 
@@ -63,12 +65,36 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json')
+        .option('-f, --file <html_file>', 'Path to index.html')
+		.option('-u, --url <website_url>', 'URL to website')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	if (program.file) {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	} else { 
+		var sys = require('util'),
+			rest = require('restler');
+
+		rest.get('http://'+program.url).on('complete', function(result) {
+		if (result instanceof Error) {
+			sys.puts('Error: ' + result.message);
+    		this.retry(5000); // try again after 5 sec
+  		} else {
+    		fs.writeFile(__dirname + '/file.txt', result, function(err) {
+				if (err) throw err;
+			});
+  		}
+		});
+		
+		var checkJson = checkHtmlFile('file.txt', program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		fs.writeFile(__dirname + '/json.txt', outJson, function(err) {
+				if (err) throw err;
+			});
+		console.log(outJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
